cc1d6432cb7908d5fab8c11934ecd8cdadac4a27
claygilk <claygilkerson@gmail.com>
2021-09-03 16:04:07 -0400

    prototype of image loading

---
+++ portfolio-site/src/assets/imageLinks.js
@@ -0,0 +1,8 @@
+
+
+    let imgLinks = [
+        "https://i.ibb.co/cYwW3xW/kavaopaque.png",
+        "https://i.ibb.co/CJjnm2k/tiefling.png"
+    ]
+
+    export default imgLinks
--- portfolio-site/src/views/Home.vue
+++ portfolio-site/src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="https://i.ibb.co/cYwW3xW/kavaopaque.png">
+    <ul>
+      <li v-for="link in links" :key="link">
+        <img :src="link" alt="">
+      </li>
+    </ul>
+    <!-- <img alt="Vue logo" src="https://i.ibb.co/cYwW3xW/kavaopaque.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,11 +13,27 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import imageLinks from '@/assets/imageLinks.js'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data(){
+    return{
+      links: null,
+    }
+  },
+  created(){
+    this.links = imageLinks
   }
 }
 </script>
+
+<style scoped>
+img{
+  width: 200px;
+  height: auto;
+}
+</style>
