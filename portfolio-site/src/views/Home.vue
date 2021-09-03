ba87ed946fee6fd87e03e6bbca46cbb10e84b8b5
claygilk <46429931+claygilk@users.noreply.github.com>
2021-09-03 15:49:03 -0400

    Update Home.vue

--- portfolio-site/src/views/Home.vue
+++ portfolio-site/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="https://icedrive.net/0/67nVRMNjUt">
+    <img alt="Vue logo" src="https://i.ibb.co/cYwW3xW/kavaopaque.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
